72c81865b4b5fe1fe41ca95a69e2f84a833af34a
Jziaty <jziaty@avans.nl>
2017-11-14 23:45:15 +0100

    Test Scene

---
+++ Assets/SoundTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour {
+
+    private AudioManager audioManager;
+
+    private void Start()
+    {
+        audioManager = AudioManager.instance;
+        if (audioManager == null)
+            Debug.LogError("No AudioManager found in the scene!");
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            audioManager.PlaySound("gunShot");
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            audioManager.PlaySound("cityAmbience");
+        }
+    }
+}
